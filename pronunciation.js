ed28ba20f019754cd9cd64bc048da0c3f243475e
Godric2003D <66315809+Godric2003D@users.noreply.github.com>
2021-05-30 18:29:34 +0530

    4th

--- pronunciation.js
+++ pronunciation.js
@@ -1,13 +1,12 @@
 function runSpeechRecognition() {
-    // get output div reference
+    
     var output = document.getElementById("output");
-    // get action element reference
     var action = document.getElementById("action");
-    // new speech recognition object
+
     var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
     var recognition = new SpeechRecognition();
 
-    // This runs when the speech recognition service starts
+
     recognition.onstart = function() {
         action.innerHTML = "<small>listening, please speak...</small>";
     };
@@ -16,8 +15,7 @@ function runSpeechRecognition() {
         action.innerHTML = "<small>stopped listening, hope you are done...</small>";
         recognition.stop();
     }
-  
-    // This runs when the speech recognition service returns result
+
     recognition.onresult = function(event) {
         var transcript = event.results[0][0].transcript;
         var confidence = event.results[0][0].confidence;
@@ -25,7 +23,7 @@ function runSpeechRecognition() {
         output.classList.remove("hide");
     };
   
-     // start recognition
+
      recognition.start();
 }
 
